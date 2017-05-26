35889d5bc1cc39ff70c0dc6e45f5d2f487595719
Neil.Sarkar <ansarkar@outlook.com>
2017-05-26 23:20:35 +1000

    hotfix key use

--- Agent.java
+++ Agent.java
@@ -182,7 +182,6 @@ public class Agent {
 		//if item use is needed, queue up its use and flag it as false in hashmap
 		if (map.get(end) == '-' && inventory.get("key") == 1){
 			moveQueue.add('u');
-			inventory.put("key", 0);
 		} else if (map.get(end) == 'T' && inventory.get("axe") == 1){
 			moveQueue.add('c');
 			//inventory.put("raft", 1);
